aae0d1ec4a90d5560d11a38b2941370e3e15685e
lutfifathilah <lutfifathila4@gmail.com>
2025-11-16 04:57:27 +0800

    p

--- hayf/settings.py
+++ hayf/settings.py
@@ -9,6 +9,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = BASE_DIR / "staticfiles_build"
+
+ 
 # ==============================================================================
 # ENVIRONMENT DETECTION
 # ==============================================================================
