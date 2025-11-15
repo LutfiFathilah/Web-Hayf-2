8586e7c2b1ab4fe44f8f6335ab69ca4dbf71537f
lutfifathilah <lutfifathila4@gmail.com>
2025-11-16 07:00:27 +0800

    bismillah

--- hayf/settings.py
+++ hayf/settings.py
@@ -92,7 +92,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
-                'dashboard.context_processors.site_settings',  # ⚠️ TAMBAHAN BARU!
+                'dashboard.context_processors.site_settings',  # ← TAMBAHAN PENTING!
             ],
         },
     },
