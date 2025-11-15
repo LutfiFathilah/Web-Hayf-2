beeeb041827a8a5a38e7ccbc70a0af975afbe310
lutfifathilah <lutfifathila4@gmail.com>
2025-11-16 05:04:36 +0800

    amin

--- hayf/settings.py
+++ hayf/settings.py
@@ -9,7 +9,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR / "staticfiles_build"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
  
 # ==============================================================================
